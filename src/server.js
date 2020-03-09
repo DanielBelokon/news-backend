3bebc93c208533c50cce394206a1b0c1bd7960b1
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-09 19:46:06 +0200

    Dev env check, misc comments

--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const articleRouter =  require("./routes/article");
-const authRouter =  require("./routes/auth");
+
+// Load routers 
+const articleRouter = require("./routes/article");
+const authRouter = require("./routes/auth");
+
+// check if in Production, if not - load dotenv
+if (process.env.NODE_ENV !== "Production") {
+    console.log("Dev env, loading dotenv");
+    require('dotenv').config();
+}
 
 const app = express();
 
+// Set up routes - pipeline start
 app.use("/article", articleRouter);
 app.use("/auth", authRouter);
 
-app.use("/", (req, res, next) => res.send({hello: "Hello World!"}));
-
-app.use((req, res, next) =>
-{
+// If we've reached this far without an error or response, the resource was not found.
+app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
 
     next(err);
 });
 
-app.use((err, req, res, next) =>
-{
+// If we've reaced this far without an error or response - something went terribly wrong.
+app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         err: err.message || "Something's wrong"
     });
 });
 
-app.listen(5000, console.log("Started listening..."));
+
+app.listen(process.env.PORT, console.log("Started listening on " + process.env.PORT));
