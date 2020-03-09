edd27d79ccf937aadae9f5251b53183e68d8264a
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-09 14:11:18 +0200

    Use IsAuthenticated, replace prev mock function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,6 @@
 const Router = require("express");
-
+const IsAuthenticated = require("../middleware/auth/userController").isAuthenticated;
 const router = Router.Router();
-
-function Authenticated(res, req, next)
-{
-    // If AUTH next ELSE Redirect /auth/login
-    console.log("Authenticating...");
-    next();
-}
 
 // User login page
 router.get('/login', (req, res) => {
@@ -20,15 +13,15 @@ return res.send('Received a POST HTTP method');
 });
 
 //
-router.get('/:id', Authenticated, (req, res) => {
+router.get('/:id', IsAuthenticated, (req, res) => {
     return res.send('Received a GET HTTP method ' + req.params.id);
   });
 
-router.put('/:id', Authenticated, (req, res) => {
+router.put('/:id', IsAuthenticated, (req, res) => {
 return res.send('Received a PUT HTTP method');
 });
 
-router.delete('/:id', Authenticated, (req, res) => {
+router.delete('/:id', IsAuthenticated, (req, res) => {
 return res.send('Received a DELETE HTTP method');
 });
 
