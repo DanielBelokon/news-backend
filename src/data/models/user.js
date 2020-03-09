34730b29528351fbdfb6be9504f6de76c1c7f142
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-09 13:57:23 +0200

    Add user model with mongoose

--- src/data/models/user.js
+++ src/data/models/user.js
@@ -1,1 +1,27 @@
 const mdb = require("mongoose");
+
+// User Schema Model
+const userSchema = mdb.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    lastLogin {
+        type: Date,
+    },
+    email: {
+        type: String
+    },
+    role: {
+        type: String,
+        default: "user"
+    }
+});
