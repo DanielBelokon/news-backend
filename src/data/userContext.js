3ada360ef514ce3f551b30154a0f7e85d208916b
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-10 20:35:58 +0200

    Hased password, finished authenticateUser

--- src/data/userContext.js
+++ src/data/userContext.js
@@ -25,7 +25,7 @@ function registerUser(username, password, email, handleError) {
     console.log("Data registering new user...")
     var user = new userModel({
         username: username,
-        password: password, //Todo: HASH!
+        password: bcrypt.hashSync(password, 10),
         email: email
     });
     user.save(function (err, user) {
@@ -43,8 +43,12 @@ function registerUser(username, password, email, handleError) {
 // User Authentication Check:
 // Method for the middle tier to authenticate a user with the data tier - using bcrypt compare & save to express session with passport on success
 // takes username, password, and passport's 'done' method which itself takes (error, user, message)
-function authenticateUser(username, password, done) {
-    const user = getUserByUsername(username);
+async function authenticateUser(username, password, done) {
+    var user = null;
+    await getUserByUsername(username, function (resultUser) {
+        user = resultUser;
+    });
+
     if (user == null) {
         // if user not found - use passport's 'done' function to return message
         return done(null, false, { message: "User not found" })
@@ -52,6 +56,7 @@ function authenticateUser(username, password, done) {
     try {
         // Compare password(plain text) with user.password (encrypted & salted from the DB object)
         if (await bcrypt.compare(password, user.password)) {
+            console.log(user.username + " logged in");
             return done(null, user);
         } else {
             return done(null, false, { message: "Password incorrect" });
