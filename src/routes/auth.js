712149d6b3a2ee29d87141768789ea06c23e428a
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-12 08:50:50 +0200

    Add /auth/protected route for testing

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,23 +7,25 @@ const router = Router.Router();
 
 // User login page
 router.get('/login', (req, res) => {
-    return res.send('Received a GET HTTP method');
-  });
+  return res.send('Received a GET HTTP method');
+});
 
 // User login post
 router.post('/login', login);
 
+router.post('/protected', isAuthenticated(), (req, res) => { res.send("Success") });
+
 //
 router.get('/:id', isAuthenticated(), (req, res) => {
-    return res.send('Received a GET HTTP method ' + req.params.id);
-  });
+  return res.send('Received a GET HTTP method ' + req.params.id);
+});
 
 router.put('/:id', isAuthenticated(), (req, res) => {
-return res.send('Received a PUT HTTP method');
+  return res.send('Received a PUT HTTP method');
 });
 
 router.delete('/:id', isAuthenticated(), (req, res) => {
-return res.send('Received a DELETE HTTP method');
+  return res.send('Received a DELETE HTTP method');
 });
 
 router.post("/register", register);
