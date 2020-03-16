bcd33e1d4b22ee4649bd059a83248c03273315a0
Daniel Belokon <daniel.belokon@gmail.com>
2020-03-16 11:27:17 +0200

    Implement articleContext

--- src/data/articleContext.js
+++ src/data/articleContext.js
@@ -1,18 +1,53 @@
 const articleModel = require("./models/article");
+const httpError = require("http-errors");
 
 function getById(id) {
-    throw new Error("Not Implemented");
+    return articleModel.findById(id).exec();
 }
 
 function getByTopic(topic) {
-    throw new Error("Not Implemented");
+    return articleModel.find({ topic: topic }).exec();
 }
 
 function getAll(id, count) {
-    throw new Error("Not Implemented");
+    return articleModel.find({}).exec();
 }
 
 function getByPage(topic) {
     throw new Error("Not Implemented");
+}
+
+function getByAuthor(authorId) {
+    return articleModel.find({userId: authorId}).exec();
+}
+
+async function create(article) {
+    if (!article) {
+        return Promise.reject();
+    }
+    console.log(article);
+    var newArticle = articleModel({
+        title: article.title,
+        body: article.body,
+        authorPseudonym: article.authorPseudonym,
+        userId: article.userId,
+        topic: article.topic || "News"
+    });
+    try {
+        var createdArticle = await newArticle.save();
+        return Promise.resolve(createdArticle);
+    } catch (err) {
+        console.error(err);
+        newErr = httpError.InternalServerError("Something went wrong with the database, try again later.");
+        return Promise.reject(newErr);
+    }
+}
+
+module.exports = {
+    create: create,
+    getById: getById,
+    getByPage: getByPage,
+    getAll: getAll,
+    getByTopic: getByTopic
 }
 
